bb45c3f168b4acce177734a34a2800d47d42c107
Pavel-1988 <102318642+Pavel-1988@users.noreply.github.com>
2022-10-09 12:19:44 +0300

    second try

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -41,11 +41,15 @@ export const Profile = ({
 }
 
 Profile.propTypes = {
-    username: PropTypes.string.isRequired,
-    tag: PropTypes.string.isRequired,
-    location: PropTypes.string.isRequired,
-    avatar: PropTypes.string.isRequired,
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.arrayOf(
+    PropTypes.exact({
     followers: PropTypes.number.isRequired,
     views: PropTypes.number.isRequired,
-    likes: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired})
+    )
+    
  }
